0fc2c8f50197d06ec253431252eb15c4cc619160
aamekst <mieko.alana@gmail.com>
2022-08-16 22:34:11 -0300

    [commit initial]

---
+++ AppRpgEtec/AppRpgEtec/Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRpgEtec.Models
+{
+    public class Usuario
+    {
+        public string Username { get; set; }    
+        public int Id { get; set; }
+
+        public string Perfil { get; set; }
+
+        public string Token { get; set; }
+
+        public byte[] Foto { get; set; }
+
+        public string PasswordString{ get; set; }
+
+    }
+}
